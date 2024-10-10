e4f13d79a30236706b64eac1dfb92e56e6a2264c
KoushalKumar22 <koushalkumar2224@gmail.com>
2024-10-10 21:01:34 +0530

    deployment

---
+++ src/main/java/com/demo/DockerComposeWithPostGr/DockerComposeWithPostGrApplication.java
@@ -0,0 +1,13 @@
+package com.demo.DockerComposeWithPostGr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerComposeWithPostGrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerComposeWithPostGrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/DockerComposeWithPostGr/DockerComposeWithPostGrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.DockerComposeWithPostGr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerComposeWithPostGrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
